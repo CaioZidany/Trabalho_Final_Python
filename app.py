b11dc70d89a82ffefb337d352db54f52c400ddc3
Caio Zidany <titidetutu11@gmail.com>
2024-05-27 20:13:02 -0300

    Versão_1.0.0

---
+++ app.py
@@ -0,0 +1,82 @@
+import os
+import csv
+from flask import Flask, render_template, redirect, url_for, request
+
+app = Flask(__name__)
+
+# Definindo a variável de ambiente para modo de desenvolvimento debug
+os.environ['FLASK_DEBUG'] = 'True'
+app.debug = os.environ.get('FLASK_DEBUG') == 'True'
+
+
+@app.route('/')
+def pagina_home():
+    return render_template('index.html')
+
+
+# Exemplo de rota para a página sobre
+@app.route('/sobre')
+def pagina_sobre():
+    return render_template('sobre.html')
+
+
+# Exemplo de rota para a página de glossário
+@app.route('/glossario')
+def glossario():
+
+    glossario_de_termos = []
+
+    with open(
+            'bd_glossario.csv',
+            newline='', encoding='utf-8') as arquivo:
+        reader = csv.reader(arquivo, delimiter=';')
+        for l in reader:
+            glossario_de_termos.append(l)
+
+    return render_template('glossario.html',
+                           glossario=glossario_de_termos)
+
+
+@app.route('/novo_termo')
+def novo_termo():
+    return render_template('adicionar_termo.html')
+
+
+@app.route('/criar_termo', methods=['POST', ])
+def criar_termo():
+    termo = request.form['termo']
+    definicao = request.form['definicao']
+
+    with open(
+            'bd_glossario.csv', 'a',
+            newline='', encoding='utf-8') as arquivo:
+        writer = csv.writer(arquivo, delimiter=';')
+        writer.writerow([termo, definicao])
+
+    return redirect(url_for('glossario'))
+
+
+@app.route('/excluir_termo/<int:termo_id>', methods=['POST'])
+def excluir_termo(termo_id):
+
+    with open('bd_glossario.csv', 'r', newline='') as file:
+        reader = csv.reader(file)
+        linhas = list(reader)
+
+    # Encontrar e excluir o termo com base no ID
+    for i, linha in enumerate(linhas):
+        if i == termo_id:
+            del linhas[i]
+            break
+
+    # Salvar as alterações de volta no arquivo
+    with open('bd_glossario.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(linhas)
+
+    return redirect(url_for('glossario'))
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
